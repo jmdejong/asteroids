6e30e5f197c897901ffcc90cd1bc5920f31b950b
W-M <w-m@gmx.us>
2015-09-28 13:29:06 +0200

    remove error in base.

--- src/aoop/asteroids/udp/Base.java
+++ src/aoop/asteroids/udp/Base.java
@@ -6,16 +6,7 @@ import java.net.UnknownHostException;
 
 public class Base {
 	public Base(){
-		byte[] buffer = new byte[65508];
-		InetAddress address;
-		try {
-			address = InetAddress.getByAddress();
-			DatagramPacket packet = new DatagramPacket(
-		    buffer, buffer.length, address, 9000);
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 
 		
 	}

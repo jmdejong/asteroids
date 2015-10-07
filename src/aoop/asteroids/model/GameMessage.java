02ac08597ca5d0e52d3c19f36f4ec32af84a3f96
W-M <w-m@gmx.us>
2015-10-07 17:39:06 +0200

    Game Message Fading.

--- src/aoop/asteroids/model/GameMessage.java
+++ src/aoop/asteroids/model/GameMessage.java
@@ -20,4 +20,27 @@ public class GameMessage {
 	public String toString(){
 		return this.message;
 	}
+	
+	public float getOpacity(){
+		if(this.isDestroyed()){
+			return 0;
+		}
+		long time = System.currentTimeMillis() - creationTime;
+		
+		if(time == 0 || time == visibleTime){
+			return 0;
+		}else if(time < visibleTime/4){
+			return (float)easingInOut(time, 0, 1 , visibleTime/4);
+		}else if (time > (visibleTime/4)*3){
+			return (float)easingInOut(visibleTime - time, 0, 1,visibleTime/4);
+		}else{
+			return 1;
+		}
+	}
+	
+	public float easingInOut(float t, float b, float c, float d){
+		float ts=(t/=d)*t;
+		float tc=ts*t;
+		return b+c*(-2*tc + 3*ts);
+	}
 }

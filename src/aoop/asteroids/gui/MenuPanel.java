c90b8d215bd2ec9ea4e7618a6376f858dfd1c2e2
W-M <w-m@gmx.us>
2015-10-20 16:30:32 +0200

    Documentation

--- src/aoop/asteroids/gui/MenuPanel.java
+++ src/aoop/asteroids/gui/MenuPanel.java
@@ -11,10 +11,8 @@ import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
-import javax.swing.Box;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
-import javax.swing.Action;
 import javax.swing.AbstractAction;
 import java.awt.CardLayout;
 import java.awt.Component;
@@ -30,7 +28,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
-
+/**
+ * The Menu Panel contains buttons to go to each of the game modes, and to quit the game.
+ * Also, up-to-date high scores of the last hour and of all time are shown.
+ * @author qqwy
+ */
 public class MenuPanel extends JPanel {
 	
 	/* 
@@ -41,33 +43,38 @@ public class MenuPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	// why are these capitalized? they're not classes. I would use either all caps or start with a lowercase letter
 	private static Color BORDERCOLOR = Color.GREEN;
 	private static Color TEXTCOLOR = Color.GREEN;
 	private static Color BACKCOLOR = Color.BLACK;
 	private static int BORDERWIDTH = 1;
-// 	private static Color TRASPARENT = new Color(0,0,0,0);
-// 	private JPanel middle;
-// 	private JPanel left;
-// 	private JPanel right;
 	
+	/**
+	 * The player can set their name in this field, before starting the game.
+	 */
 	private JTextField nameField;
-	
-	
-	
-	private Map<String,JButton> buttons = new HashMap<String,JButton>();
-	
-// 	private JPanel mainPanel;
-// 	private JPanel addressPanel;
-	
-// 	private JButton connectButton;
 
+	/**
+	 * A map of all buttons created in this menu, the keys being their button text.
+	 * Used to add EventListeners to them after they have been created, by referencing them by their button text.
+	 */
+	private Map<String,JButton> buttons = new HashMap<String,JButton>();
+
+	/**
+	 * The player can enter a server location to connect to in this field.
+	 */
 	private JTextField addressField;
 
+	/**
+	 * A reference to the high scores panel of all time, to enable refreshing the contained data.
+	 */
 	private JPanel allTimeHighScores;
+	/**
+	 * A reference to the high scores panel of the last hour, to enable refreshing the contained data.
+	 */
 	private JPanel lastHourHighScores;
 	
 	
+	@SuppressWarnings("serial")
 	public MenuPanel() {
 		
 		// Set color of menu panel background
@@ -75,12 +82,9 @@ public class MenuPanel extends JPanel {
 		
 		// Make a box layout of vertical buttons, rigid area dimensions determine it's location on the y-axis
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
-		
 		addTitle(this, "ASTEROIDS");
 		
 		nameField = addInput(this, Utils.generateName(), "Name:");
-		
 
 		this.add(Box.createRigidArea(new Dimension(0,50)));
 		
@@ -125,7 +129,12 @@ public class MenuPanel extends JPanel {
 		
 	}
 	
-	
+	/**
+	 * Creates a rigid, empty box. Used for positioning the other elements on the panel.
+	 * @param size the width and height of the box
+	 * @param axis attempts to align the box. Pass in a BoxLayout enumerable.
+	 * @see BoxLayout
+	 */
 	private JPanel makeBox(Dimension size, int axis){
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, axis));
@@ -139,6 +148,11 @@ public class MenuPanel extends JPanel {
 		return panel;
 	}
 	
+	/**
+	 * Creates a large title JLabel and adds it to the given panel.
+	 * @param panel the panel to add the title to
+	 * @param titleText the text to use as title
+	 */
 	private void addTitle(JPanel panel, String titleText){
 		
 		this.add(Box.createRigidArea(new Dimension(0, 20)));
@@ -154,7 +168,13 @@ public class MenuPanel extends JPanel {
 		panel.add(Box.createRigidArea(new Dimension(0, 30)));
 	}
 
-	
+	/**
+	 * Adds an input field to the specified panel
+	 * @param panel the panel to add the input to
+	 * @param defaultText the text to use until it changes
+	 * @param title a title to use in small in the label.
+	 * @return the created input field
+	 */
 	private JTextField addInput(JPanel panel, String defaultText, String title){
 		JTextField inputField = makeInput(defaultText, title, panel.getFont());
 		panel.add(inputField);
@@ -164,10 +184,16 @@ public class MenuPanel extends JPanel {
 		return inputField;
 	}
 	
-	
+	/**
+	 * Adds an input field to the specified panel
+	 * @param panel the panel to add the input to
+	 * @param defaultText the text to use until it changes
+	 * @param title a title to use in small in the label.
+	 * @param f a font to use for the text in this input field
+	 * @return the created input field
+	 */
 	private JTextField makeInput(String defaultText, String title, Font f){
 		JTextField inputField = new JTextField();
-// 		inputField.setAlignmentY( Component.TOP_ALIGNMENT );
 		inputField.setMaximumSize( new Dimension(120,80) );
 		inputField.setBackground(MenuPanel.BACKCOLOR);
 		inputField.setForeground(MenuPanel.TEXTCOLOR);
@@ -185,12 +211,25 @@ public class MenuPanel extends JPanel {
 		return inputField;
 	}
 	
+	/**
+	 * Creates an input field without adding it to any panel.
+	 * @param defaultText the text to use until it changes
+	 * @param title a title to use in small in the label.
+	 * @return the created input field
+	 */
 	private JTextField makeInput(String defaultText, String title){
 		return makeInput(defaultText, title, new Font(null));
 	}
 	
 	
-	
+	/**
+	 * Creates a button with the given text and adds it to the given panel.<br/><br/>
+	 * Note that `text` can also be used to reference the button later on when setting a button action.
+	 * @param panel the panel to add the button to
+	 * @param text the text the button should contain.
+	 * @return the created button
+	 * @see #setButtonAction(String, ActionListener)
+	 */
 	private JButton makeButton(JPanel panel, String text){
 		
 		JButton button = new JButton(text);
@@ -200,8 +239,6 @@ public class MenuPanel extends JPanel {
 		}
 		this.buttons.put(text, button);
 		
-		
-// 		button.setFont(Font.getFont("Courier"));
 		button.setAlignmentX( Component.CENTER_ALIGNMENT );
 		button.setMaximumSize( new Dimension(180,50) );
 		button.setBackground(MenuPanel.BACKCOLOR);
@@ -218,24 +255,55 @@ public class MenuPanel extends JPanel {
 		return button;
 
 	}
+	
+	/**
+	 * Creates a button with the given text and adds it to the given panel.<br/><br/>
+	 * Note that `text` can also be used to reference the button later on when setting a button action.
+	 * @param panel the panel to add the button to
+	 * @param text the text the button should contain.
+	 * @param action an ActionListener to execute when a user interacts with the button.
+	 * @return the created button
+	 * @see #setButtonAction(String, ActionListener)
+	 */
 	private JButton makeButton(JPanel panel, String text, ActionListener action){
 		JButton button = this.makeButton(panel, text);
 		this.setButtonAction(text, action);
 		return button;
 	}
+	
+	/**
+	 * Creates a button with the given text
+	 * @return the created button
+	 * @see #setButtonAction(String, ActionListener)
+	 */
 	private JButton makeButton(String text){
 		return this.makeButton(null,text);
 	}
 	
+	/**
+	 * Creates a button with the given text, and an ActionListener to execute when a user interacts with the button.
+	 * @return the created button
+	 * @see #setButtonAction(String, ActionListener)
+	 */
 	private JButton makeButton(String text, ActionListener action){
 		return this.makeButton(null,text,action);
 	}
 	
-	
+	/**
+	 * Creates a new panel and adds the given highscores to it.
+	 * These are then formatted to be a nice list.
+	 * @return the created container
+	 */
 	private JPanel addHighScores(String scoreListName, List<PlayerScore> scores){
 		JPanel container = makeBox(null, BoxLayout.Y_AXIS);
 		return addHighScores(scoreListName, scores, container);
 	}
+	
+	/**
+	 * Adds the given highscores to an already existing container.
+	 * These are then formatted to be a nice list.
+	 * @return the edited container
+	 */
 	private JPanel addHighScores(String scoreListName, List<PlayerScore> scores, JPanel container){
 		
 		container.setBorder(new TitledBorder(BorderFactory.createLineBorder(MenuPanel.BORDERCOLOR, MenuPanel.BORDERWIDTH), scoreListName, TitledBorder.CENTER, TitledBorder.TOP, container.getFont(), MenuPanel.TEXTCOLOR));
@@ -262,6 +330,9 @@ public class MenuPanel extends JPanel {
 		return container;
 	}
 	
+	/**
+	 * Reloads the high-scores and displays the new versions on this panel.
+	 */
 	public void reloadHighScores(){
 		this.allTimeHighScores.removeAll();
 		this.lastHourHighScores.removeAll();
@@ -269,6 +340,13 @@ public class MenuPanel extends JPanel {
 		addHighScores("Last Hour High Scores",HighScores.getInstance().getLastHourHighScores(), this.lastHourHighScores);
 	}
 	
+	/**
+	 * Adds the given ActionListener to a button with the given button_name, that should have been created using makeButton() before.
+	 * @see #makeButton(String)
+	 * @see #makeButton(JPanel, String)
+	 * @see #makeButton(String, ActionListener)
+	 * @see #makeButton(JPanel, String, ActionListener)
+	 */
 	public void setButtonAction(String button_name, ActionListener action){
 		JButton button = buttons.get(button_name);
 		if(button == null){
@@ -277,6 +355,14 @@ public class MenuPanel extends JPanel {
 		button.addActionListener(action);
 	}
 	
+	/**
+	 * Creates a JPanel that might contain multiple child components
+	 * @param size width and height of the panel
+	 * @param axis the alignment BoxLayout enumerable.
+	 * @param child_components a list of zero or more child components.
+	 * @see BoxLayout
+	 * @return the created composite panel
+	 */
 	public JPanel makeCompositePanel(Dimension size, int axis, JComponent... child_components){
 		JPanel panel = makeBox(size, axis);
 		for(JComponent c : child_components){
@@ -290,11 +376,18 @@ public class MenuPanel extends JPanel {
 		return panel;
 	}
 	
-	
+	/**
+	 * @see #nameField
+	 * @return the entered value in the nameField JTextField
+	 */
 	public String getPlayerName(){
 		return this.nameField.getText();
 	}
 	
+	/**
+	 * @see #addressField
+	 * @return the entered value in the addressField JTextField
+	 */
 	public String getAddress(){
 		return this.addressField.getText();
 	}
--- src/aoop/asteroids/gui/SpaceshipController.java
+++ src/aoop/asteroids/gui/SpaceshipController.java
@@ -5,9 +5,19 @@ import java.awt.event.KeyListener;
 import java.util.HashMap;
 import java.util.Map;
 
+
+/**
+ * This class listens to key presses, and keeps track of them after the actual key-events have happened.
+ * To reduce code-duplication, a map is used to keep track of individual key states.
+ * @author qqwy
+ *
+ */
 public class SpaceshipController implements KeyListener{
 	private Map<Integer,Boolean> keyStates;
 	
+	/**
+	 * Initializes all key states to false.
+	 */
 	public SpaceshipController(){
 		this.keyStates = new HashMap<Integer,Boolean>();
 		this.keyStates.put(KeyEvent.VK_UP,    false);
@@ -16,6 +26,9 @@ public class SpaceshipController implements KeyListener{
 		this.keyStates.put(KeyEvent.VK_SPACE, false);
 	}
 
+	/**
+	 * When a key is pressed, update its state to 'true'.
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if(this.keyStates.containsKey(e.getKeyCode())){
@@ -24,6 +37,9 @@ public class SpaceshipController implements KeyListener{
 		}
 	}
 
+	/**
+	 * When a key is released, update its state to 'false'.
+	 */
 	@Override
 	public void keyReleased(KeyEvent e) {
 		if(this.keyStates.containsKey(e.getKeyCode())){
@@ -33,23 +49,41 @@ public class SpaceshipController implements KeyListener{
 		
 	}
 
+	
+	/**
+	 * We are not interested in typed events.
+	 */
 	@Override
 	public void keyTyped(KeyEvent e) {}
 
+	/**
+	 * @return if the 'up' key is currently pressed, which makes the spaceship go forward.
+	 */
 	public boolean isUp() {
 		return keyStates.get(KeyEvent.VK_UP);
 	}
 	
+	/**
+	 * @return if the 'right' key is currently pressed, which makes the spaceship rotate right.
+	 */
 	public boolean isRight() {
 		return keyStates.get(KeyEvent.VK_RIGHT);
 	}
 
+	/**
+	 * @return if the 'left' key is currently pressed, which makes the spaceship rotate left.
+	 */
+	public boolean isLeft() {
+		return keyStates.get(KeyEvent.VK_LEFT);
+	}
+	
+	/**
+	 * @return if the 'space' key is currently pressed, which makes the spaceship fire a bullet.
+	 */
 	public boolean isFire() {
 		return keyStates.get(KeyEvent.VK_SPACE);
 	}
 
-	public boolean isLeft() {
-		return keyStates.get(KeyEvent.VK_LEFT);
-	}
+	
 
 }

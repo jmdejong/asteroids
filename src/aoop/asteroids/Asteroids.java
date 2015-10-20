0921a021bad3dc7565d862c6b60886a678b4e07e
W-M <w-m@gmx.us>
2015-10-20 14:05:53 +0200

    reset default width/height

--- src/aoop/asteroids/Asteroids.java
+++ src/aoop/asteroids/Asteroids.java
@@ -38,8 +38,8 @@ public class Asteroids {
 	
 	
 	
-	public static int worldWidth = 1600;
-	public static int worldHeight = 960;
+	public static int worldWidth = 800;
+	public static int worldHeight = 700;
 	
 	
 	/** 

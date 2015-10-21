34817b69b9d1532d0840d07678bb57acd75da52d
JM de Jong <troido@hotmail.com>
2015-10-21 15:19:57 +0200

    updated build.xml and run.sh to match json_simple as a lib

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 BASEDIR="$(dirname $0)"
 cd $BASEDIR
-java -cp lib/javax.persistence_2.0.0.jar:lib/objectdb.jar:bin/ aoop.asteroids.Asteroids
+java -cp lib/javax.persistence_2.0.0.jar:lib/objectdb.jar:lib/json_simple.jar:bin/ aoop.asteroids.Asteroids
